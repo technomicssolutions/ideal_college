6b2cd5ef97972078ffa6bcf2ff0f611069234f5c
geethu <geethu@technomicssolutions.com>
2014-08-26 12:32:57 +0530

    Removed duplicate entry for address in Student model

--- ideal_college/academic/models.py
+++ ideal_college/academic/models.py
@@ -13,7 +13,6 @@ class Student(models.Model):
 	qualified_exam = models.ManyToManyField(QualifiedExam, null=True, blank=True)
 	technical_qualification = models.ManyToManyField(TechnicalQualification, null=True, blank=True)
 	dob = models.DateField('Date of Birth',null=True, blank=True)
-	address= models.CharField('Student Address',null=True, blank=True, max_length=200)
 	mobile_number= models.CharField('Mobile Number',null=True, blank=True, max_length=200)
 	land_number= models.CharField('Land Number',null=True, blank=True, max_length=200)
 	email = models.CharField('Email',null=True, blank=True, max_length=200)
