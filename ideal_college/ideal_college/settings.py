0c86229bfd5346ad198b58b22069053649edbd1e
Anoop Kumar <anoopkumar@technomicssolutions.com>
2014-08-26 14:31:51 +0530

    settings

--- ideal_college/ideal_college/settings.py
+++ ideal_college/ideal_college/settings.py
@@ -1,4 +1,5 @@
 # Django settings for ideal_college project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -51,11 +52,11 @@ USE_TZ = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
 MEDIA_ROOT = 'media'
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = '/site_media/'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
