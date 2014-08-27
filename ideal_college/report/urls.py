07cc3e9939e020f4f8acb6b88779db40b6b21aeb
Anoop Kumar <anoopkumar@technomicssolutions.com>
2014-08-27 16:48:40 +0530

    fee collect report

--- ideal_college/report/urls.py
+++ ideal_college/report/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from report.views import OutstandingFeesListReport, IdcardReport, CommonFeeReport
+from report.views import OutstandingFeesListReport, IdcardReport, CommonFeeReport, FeeCollectedReport
 
 urlpatterns = patterns('',
 	url(r'^outstanding_fees_report/$',login_required(OutstandingFeesListReport.as_view()), name='outstanding_fees_report'),
 	url(r'^id_card/$',login_required(IdcardReport.as_view()), name='id_card'),
 	url(r'^common_fee_report/$',login_required(CommonFeeReport.as_view()), name='common_fee_report'),
+	url(r'^fee_collected/$',login_required(FeeCollectedReport.as_view()), name='fee_collected'),
 )
